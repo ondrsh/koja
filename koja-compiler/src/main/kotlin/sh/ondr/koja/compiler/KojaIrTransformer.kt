8551fa0a44fca96c7d3a63ef6a3c353946c6003e
Andreas Toth <me@ondr.sh>
2025-01-16 19:35:43 +0200

    Hook into kmcp

--- koja-compiler/src/main/kotlin/sh/ondr/koja/compiler/KojaIrTransformer.kt
+++ koja-compiler/src/main/kotlin/sh/ondr/koja/compiler/KojaIrTransformer.kt
@@ -6,9 +6,13 @@ import org.jetbrains.kotlin.backend.common.lower.irBlock
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.ir.builders.irGetObject
 import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.symbols.UnsafeDuringIrConstructionAPI
+import org.jetbrains.kotlin.ir.util.constructedClass
 import org.jetbrains.kotlin.ir.util.fqNameWhenAvailable
+import org.jetbrains.kotlin.ir.util.packageFqName
+import org.jetbrains.kotlin.ir.util.parentClassOrNull
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
@@ -34,7 +38,7 @@ class KojaIrTransformer(
 		val functionFqName = callee.fqNameWhenAvailable
 
 		if (functionFqName in functionsToInject) {
-			val initializerSymbol = pluginContext.referenceClass(initializerClassId) ?: error("Could not find KojaInitializer class")
+			val initializerSymbol = pluginContext.referenceClass(initializerClassId) ?: error("Could not find KojaInitializer")
 
 			// Insert a reference to KojaInitializer before the call
 			val builder = DeclarationIrBuilder(
@@ -45,6 +49,33 @@ class KojaIrTransformer(
 			)
 
 			// Build a small IR block: first get KojaInitializer, then do the original call
+			return builder.irBlock(expression = expression) {
+				+irGetObject(initializerSymbol)
+				+expression
+			}
+		}
+
+		return expression
+	}
+
+	override fun visitConstructorCall(expression: IrConstructorCall): IrExpression {
+		expression.transformChildrenVoid()
+
+		val callee = expression.symbol.owner
+		val isNamedBuilder = callee.constructedClass.name.asString() == "Builder"
+		val isInServer = callee.constructedClass.parentClassOrNull?.name?.asString() == "Server"
+		val isInRuntimePackage = callee.constructedClass.parentClassOrNull?.packageFqName?.asString() == "sh.ondr.kmcp.runtime"
+		if (isNamedBuilder && isInServer && isInRuntimePackage) {
+			val initializerSymbol = pluginContext.referenceClass(initializerClassId) ?: error("Could not find KojaInitializer")
+
+			val builder =
+				DeclarationIrBuilder(
+					generatorContext = pluginContext,
+					symbol = callee.symbol,
+					startOffset = expression.startOffset,
+					endOffset = expression.endOffset,
+				)
+
 			return builder.irBlock(expression = expression) {
 				+irGetObject(initializerSymbol)
 				+expression
