1c7c31495d5596c2cf2ce6f5f3d0a953f6e5de4a
Andreas Toth <me@ondr.sh>
2025-06-20 13:32:33 +0300

    Parameterize Kotlin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,31 @@ dependencyResolutionManagement {
 	repositories {
 		mavenCentral()
 	}
+	
+	versionCatalogs {
+		// Configure the existing libs catalog
+		configureEach {
+			if (name == "libs") {
+				// Allow overriding Kotlin version for testing
+				val kotlinVersionOverride = providers
+					.gradleProperty("test.kotlin.version")
+					.orElse(providers.environmentVariable("TEST_KOTLIN_VERSION"))
+				
+				if (kotlinVersionOverride.isPresent) {
+					val versionValue = kotlinVersionOverride.get()
+					println("Overriding Kotlin version to $versionValue for testing")
+					
+					// Override the kotlin version
+					version("kotlin", versionValue)
+					
+					// Also need to update the plugin versions to match
+					plugin("kotlin-multiplatform", "org.jetbrains.kotlin.multiplatform").version(versionValue)
+					plugin("kotlin-jvm", "org.jetbrains.kotlin.jvm").version(versionValue)
+					plugin("kotlin-serialization", "org.jetbrains.kotlin.plugin.serialization").version(versionValue)
+				}
+			}
+		}
+	}
 }
 
 rootProject.name = "koja"
